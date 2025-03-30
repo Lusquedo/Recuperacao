34b9afd10d0fe0e94f73bb48301f030726734bbd
Lucas Ramos da Silva <Luscas.Ramos.da.Silva@gmail.com>
2025-03-30 14:06:26 -0300

    Loja.java

--- Sistema de Produtos em uma Loja/Loja.java
+++ Sistema de Produtos em uma Loja/Loja.java
@@ -1,44 +1,44 @@
 class Produto {
-    protected String nome;
-    protected double preco;
+    String nome;
+    double preco;
 
-    public Produto(String nome, double preco) {
+    Produto(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
     }
 
-    public void exibirDetalhes() {
+    void exibirDetalhes() {
         System.out.println("Produto: " + nome);
         System.out.println("Preço: R$ " + preco);
     }
 }
 
 class ProdutoComDesconto extends Produto {
-    private double desconto;
+    double desconto;
 
-    public ProdutoComDesconto(String nome, double preco, double desconto) {
+    ProdutoComDesconto(String nome, double preco, double desconto) {
         super(nome, preco);
         this.desconto = desconto;
     }
 
-    public void exibirDetalhesComDesconto() {
-        double precoComDesconto = preco - (preco * (desconto / 100));
+    void exibirDetalhes() {
+        double precoFinal = preco - (preco * desconto / 100);
         System.out.println("Produto: " + nome);
         System.out.println("Preço original: R$ " + preco);
         System.out.println("Desconto: " + desconto + "%");
-        System.out.println("Preço com desconto: R$ " + precoComDesconto);
+        System.out.println("Preço com desconto: R$ " + precoFinal);
     }
 }
 
 public class Loja {
     public static void main(String[] args) {
-        Produto produto1 = new Produto("Notebook", 4600.00);
-        ProdutoComDesconto produto2 = new ProdutoComDesconto("Notebook", 4600.00, 10);
+        Produto p1 = new Produto("Notebook", 4600.00);
+        ProdutoComDesconto p2 = new ProdutoComDesconto("Notebook", 4600.00, 10);
 
         System.out.println("=== Produto sem desconto ===");
-        produto1.exibirDetalhes();
-
+        p1.exibirDetalhes();
+        
         System.out.println("\n=== Produto com desconto ===");
-        produto2.exibirDetalhesComDesconto();
+        p2.exibirDetalhes();
     }
 }
