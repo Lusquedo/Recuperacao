c3a4ac267af943c72131d1df684a241bd9e8e016
Lucas Ramos da Silva <Luscas.Ramos.da.Silva@gmail.com>
2025-03-30 14:11:16 -0300

    SistemaFuncionarios.java

--- Sistema de Funcionários/SistemaFuncionarios.java
+++ Sistema de Funcionários/SistemaFuncionarios.java
@@ -1,15 +1,15 @@
 class Funcionario {
-    protected String nome;
-    protected int idade;
-    protected double salario;
+    String nome;
+    int idade;
+    double salario;
 
-    public Funcionario(String nome, int idade, double salario) {
+    Funcionario(String nome, int idade, double salario) {
         this.nome = nome;
         this.idade = idade;
         this.salario = salario;
     }
 
-    public void exibirInformacoes() {
+    void exibirInformacoes() {
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
         System.out.println("Salário: R$ " + salario);
@@ -17,43 +17,43 @@ class Funcionario {
 }
 
 class Gerente extends Funcionario {
-    private String departamento;
+    String departamento;
 
-    public Gerente(String nome, int idade, double salario, String departamento) {
+    Gerente(String nome, int idade, double salario, String departamento) {
         super(nome, idade, salario);
         this.departamento = departamento;
     }
 
-    public void exibirInformacoesGerente() {
-        exibirInformacoes();
+    void exibirInformacoes() {
+        super.exibirInformacoes();
         System.out.println("Departamento: " + departamento);
     }
 }
 
 class Desenvolvedor extends Funcionario {
-    private String linguagemProgramacao;
+    String linguagemProgramacao;
 
-    public Desenvolvedor(String nome, int idade, double salario, String linguagemProgramacao) {
+    Desenvolvedor(String nome, int idade, double salario, String linguagemProgramacao) {
         super(nome, idade, salario);
         this.linguagemProgramacao = linguagemProgramacao;
     }
 
-    public void exibirInformacoesDesenvolvedor() {
-        exibirInformacoes();
+    void exibirInformacoes() {
+        super.exibirInformacoes();
         System.out.println("Linguagem de Programação: " + linguagemProgramacao);
     }
 }
 
 public class SistemaFuncionarios {
     public static void main(String[] args) {
-        Gerente gerente = new Gerente("João Silva", 60, 9000.00, "T.I");
-        Desenvolvedor dev = new Desenvolvedor("Maria Santos", 30, 6000.00, "Java");
+        Gerente g = new Gerente("João Silva", 60, 9000.00, "T.I");
+        Desenvolvedor d = new Desenvolvedor("Maria Santos", 30, 6000.00, "Java");
 
         System.out.println("Informações do Gerente:");
-        gerente.exibirInformacoesGerente();
+        g.exibirInformacoes();
         System.out.println();
         
         System.out.println("Informações do Desenvolvedor:");
-        dev.exibirInformacoesDesenvolvedor();
+        d.exibirInformacoes();
     }
 }
